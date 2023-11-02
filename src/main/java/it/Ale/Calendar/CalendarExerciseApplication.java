df83793648c600dd961639a4a90045b883c56f17
Alex <alex97av@gmail.com>
2023-11-02 17:00:23 +0100

    create a github repo, project structurebase

---
+++ src/main/java/it/Ale/Calendar/CalendarExerciseApplication.java
@@ -0,0 +1,13 @@
+package it.Ale.Calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/Ale/Calendar/CalendarExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package it.Ale.Calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
